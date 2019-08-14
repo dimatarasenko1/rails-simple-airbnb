19c6c838c420133f127fca83cd6f3476da4ea659
Dima Tarasenko <taras3nko.dima@gmail.com>
2019-08-14 16:17:36 +0100

    completed challenge - added all the basic actions needed

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -12,4 +12,24 @@ class FlatsController < ApplicationController
     @flat.destroy
     redirect_to flats_path
   end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.valid?
+      @flat.save
+      redirect_to flats_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
 end
